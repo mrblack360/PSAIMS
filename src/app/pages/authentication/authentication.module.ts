249c5981d977847f938e5d1568882bcaf13139df
mrblack360 <rmaswi360@outlook.com>
2021-01-22 13:45:58 +0300

    Update authentication routes

--- src/app/pages/authentication/authentication.module.ts
+++ src/app/pages/authentication/authentication.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './views/login/login.component';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
 
-
+// Material Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    AuthenticationRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
