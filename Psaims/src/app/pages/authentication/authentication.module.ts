426137b11d23118128e0c771f80fbccd384bd980
mrblack360 <rmaswi360@outlook.com>
2021-01-27 21:14:01 +0300

    fix: email binding

--- Psaims/src/app/pages/authentication/authentication.module.ts
+++ Psaims/src/app/pages/authentication/authentication.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { LoginComponent } from './views/login/login.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { ForgetPasswordComponent } from './views/forget-password/forget-password.component';
 
 // Material Modules
 import { MatCardModule } from '@angular/material/card';
@@ -10,12 +13,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-import { ForgetPasswordComponent } from './views/forget-password/forget-password.component';
-
 @NgModule({
   declarations: [LoginComponent, ForgetPasswordComponent],
   imports: [
     CommonModule,
+    FormsModule,
     AuthenticationRoutingModule,
     MatCardModule,
     MatInputModule,
