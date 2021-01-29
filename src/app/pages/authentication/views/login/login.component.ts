374d74a4619ba6d813617a7e2ebdf6c535a28911
mrblack360 <rmaswi360@outlook.com>
2021-01-29 23:12:28 +0300

    fix: login router

--- src/app/pages/authentication/views/login/login.component.ts
+++ src/app/pages/authentication/views/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this._router.navigate(['/home']);
   }
-
 }
