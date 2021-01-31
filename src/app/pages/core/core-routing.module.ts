705dc4129a93e08c3d577f2474ce5e0917a2c4e7
mrblack360 <rmaswi360@outlook.com>
2021-01-31 17:07:30 +0300

    Create students component

--- src/app/pages/core/core-routing.module.ts
+++ src/app/pages/core/core-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// Components
 import { NotFoundComponent } from '../not-found/not-found.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { HomeComponent } from './views/home/home.component';
+import { StudentsComponent } from './views/students/students.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,7 @@ const routes: Routes = [
     children: [
       { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
       { path: 'dashboard', component: DashboardComponent },
+      { path: 'students', component: StudentsComponent },
       { path: '**', component: NotFoundComponent },
     ],
   },
