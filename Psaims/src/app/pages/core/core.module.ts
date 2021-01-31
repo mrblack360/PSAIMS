cbd1d813d87083c1ce26238ec817338878c8ef41
mrblack360 <rmaswi360@outlook.com>
2021-01-31 12:53:26 +0300

    Create dashboard component

--- Psaims/src/app/pages/core/core.module.ts
+++ Psaims/src/app/pages/core/core.module.ts
@@ -3,8 +3,23 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './views/home/home.component';
 import { CoreRoutingModule } from './core-routing.module';
 
+// Material Modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+// NgChartjsModule
+import { NgChartjsModule } from 'ng-chartjs';
+
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, CoreRoutingModule],
+  declarations: [HomeComponent, DashboardComponent],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    NgChartjsModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
 })
 export class CoreModule {}
--- Psaims/src/app/pages/core/views/home/home.component.ts
+++ Psaims/src/app/pages/core/views/home/home.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  tabs = [
+    { name: 'Dashboard', permission: 3, link: 'dashboard' },
+    { name: 'Students', permission: 3, link: 'students' },
+    { name: 'Teachers', permission: 2, link: 'teachers' },
+    { name: 'Classes', permission: 2, link: 'classes' },
+    { name: 'Subjects', permission: 2, link: 'subjects' },
+    { name: 'Assessments', permission: 1, link: 'assessments' },
+    { name: 'Reports', permission: 1, link: 'reports' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
