6b0f252c529ed41ffed12dca8b113c578440d764
mrblack360 <rmaswi360@outlook.com>
2021-01-31 12:53:26 +0300

    Create dashboard component

--- src/app/pages/core/core-routing.module.ts
+++ src/app/pages/core/core-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '**', component: NotFoundComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/pages/core/views/dashboard/dashboard.component.ts
@@ -0,0 +1,91 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+})
+export class DashboardComponent implements OnInit {
+  chart: any;
+  doughnut: any;
+  pie: any;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.chart = new Chart('students_per_class', {
+      type: 'bar',
+      options: {
+        responsive: true,
+        title: { display: true, text: 'Students In each Class' },
+        legend: {
+          display: true,
+        },
+      },
+      data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7'],
+        datasets: [
+          {
+            type: 'bar',
+            label: 'Students',
+            data: [123, 123, 23, 453, 12, 90, 200, 150],
+            backgroundColor: 'rgba(150,204,37,1)',
+            borderColor: 'rgba(255,0,255,0.5)',
+          },
+        ],
+      },
+    });
+    this.doughnut = new Chart('students_performance', {
+      type: 'doughnut',
+      options: {
+        responsive: true,
+        title: { display: true, text: 'Students Perfomance' },
+      },
+      data: {
+        datasets: [
+          {
+            data: [45, 10, 5, 25, 15],
+            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue'],
+            label: 'Students Performance',
+          },
+        ],
+        labels: ['A', 'B', 'C', 'D', 'E'],
+      },
+    });
+    this.pie = new Chart('teachers_performance', {
+      type: 'pie',
+      options: {
+        responsive: true,
+        title: { display: true, text: 'Teachers Perfomance' },
+      },
+      data: {
+        datasets: [
+          {
+            data: [45, 10, 5, 25, 15],
+            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue'],
+            label: 'Students Performance',
+          },
+        ],
+        labels: ['Shangwe', 'Chris', 'Marlon', 'Mawazo', 'Lwanga'],
+      },
+    });
+    this.pie = new Chart('subjects_performance', {
+      type: 'pie',
+      options: {
+        responsive: true,
+        title: { display: true, text: 'Subjects Perfomance' },
+      },
+      data: {
+        datasets: [
+          {
+            data: [45, 10, 5, 25, 15],
+            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue'],
+            label: 'Students Performance',
+          },
+        ],
+        labels: ['Chemistry', 'Physics', 'Mathematics', 'Azim', 'Angelo'],
+      },
+    });
+  }
+}
