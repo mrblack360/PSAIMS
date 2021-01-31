e1fe84ebf812547681ed6a49eabbb60c9ccb5b12
mrblack360 <rmaswi360@outlook.com>
2021-01-31 17:07:30 +0300

    Create students component

--- Psaims/src/app/pages/core/core.module.ts
+++ Psaims/src/app/pages/core/core.module.ts
@@ -6,20 +6,33 @@ import { CoreRoutingModule } from './core-routing.module';
 // Material Modules
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 // NgChartjsModule
 import { NgChartjsModule } from 'ng-chartjs';
 
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { StudentsComponent } from './views/students/students.component';
 
 @NgModule({
-  declarations: [HomeComponent, DashboardComponent],
+  declarations: [HomeComponent, DashboardComponent, StudentsComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
     NgChartjsModule,
     MatButtonModule,
     MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSnackBarModule,
   ],
 })
 export class CoreModule {}
