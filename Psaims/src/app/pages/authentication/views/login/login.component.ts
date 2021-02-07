5913b08a5dfa2f4e85e59348124ee6056d424eab
mrblack360 <rmaswi360@outlook.com>
2021-02-07 22:03:34 +0300

    Implemented authentication

--- Psaims/src/app/pages/authentication/views/login/login.component.ts
+++ Psaims/src/app/pages/authentication/views/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { LoginService } from 'src/app/shared/services/login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,11 +9,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private _router: Router) {}
+  username = '';
+  password = '';
+  constructor(
+    public router: Router,
+    public loginService: LoginService,
+    public snackBar: MatSnackBar
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }
 
   ngOnInit(): void {}
 
   login() {
-    this._router.navigate(['/home']);
+    // this.loginService.login(this.username, this.password).subscribe(
+    //   (data) => {
+    //     this.snackBar.open('Successfully Loged in as ' + data.username, 'OK', {
+    //       duration: 3000,
+    //       horizontalPosition: 'center',
+    //     });
+    this.router.navigate(['../home']);
+    //   },
+    //   (err) => {
+    //     this.snackBar.open('Ooooops! Something is not right!', 'OK', {
+    //       duration: 3000,
+    //       horizontalPosition: 'center',
+    //     });
+    //     throw err;
+    //   }
+    // );
   }
 }
