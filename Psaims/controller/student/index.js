4d3bc5a7f610556042bed5ed653308ce4e0c598a
mrblack360 <rmaswi360@outlook.com>
2021-02-16 22:50:36 +0300

    fix: get student error

--- Psaims/controller/student/index.js
+++ Psaims/controller/student/index.js
@@ -1,14 +1,15 @@
-var dbConnection = require('../../config/mysql')
+var dbConnection = require("../../config/mysql");
 
 //add student
-exports.addStudent =  (req, res) => {
-
-  const {firstName, middleName, lastName, gender} = req.body
+exports.addStudent = (req, res) => {
+  const { firstName, middleName, lastName, gender } = req.body;
   try {
-    dbConnection.query("INSERT INTO student (firstName, middleName, lastName, gender, class) VALUES (?, ?, ?, ?, ?)",
-    [firstName, middleName, lastName, gender, req.body.class],
+    dbConnection.query(
+      "INSERT INTO student (firstName, middleName, lastName, gender, class) VALUES (?, ?, ?, ?, ?)",
+      [firstName, middleName, lastName, gender, req.body.name],
       (err, results, fields) => {
-        if (err) res.status(417).send({ message: "Failed to add student" });
+        if (err)
+          res.status(417).send({ message: "Failed to add student" + results });
         if (results)
           res.status(200).send({ message: "Seccessfully added a student" });
       }
@@ -16,14 +17,15 @@ exports.addStudent =  (req, res) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 //delete student
 exports.deleteStudent = (req, res) => {
-  const {id} = req.body
+  const { id } = req.body;
   try {
     dbConnection.query(
-      "DELETE FROM student WHERE id=?" ,[id],
+      "DELETE FROM student WHERE id=?",
+      [id],
       (err, results, fields) => {
         if (err) res.status(417).send({ message: "Failed to delete student" });
         if (results) res.status(200).send({ message: "Successfully Deleted" });
@@ -32,14 +34,15 @@ exports.deleteStudent = (req, res) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 //get a student
 exports.getOneStudent = (req, res) => {
-  const {id} = req.params
+  const { id } = req.params;
   try {
     dbConnection.query(
-      "SELECT * FROM student where id=?" ,[id],
+      "SELECT * FROM student where id=?",
+      [id],
       (err, results, fields) => {
         if (err) res.status(502).send({ message: "Service Unavailable" });
         if (results.length === 0)
@@ -50,30 +53,32 @@ exports.getOneStudent = (req, res) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 // get all students
-exports.getAllStudents =  (req, res) => {
+exports.getAllStudents = (req, res) => {
   try {
-    dbConnection.query("SELECT * FROM student", (err, results, fields) => {
-      if (err) res.status(502).send({ message: "Service Unavailable" });
-      if (results) res.json(results);
-    });
+    dbConnection.query(
+      "SELECT student.id as id, firstName, middleName, lastName, gender, class, class.id as classId, name, year FROM student, class WHERE student.class=class.id",
+      (err, results, fields) => {
+        if (err) res.status(502).send({ message: "Service Unavailable" });
+        if (results) res.json(results);
+      }
+    );
   } catch (err) {
     console.log(err);
   }
-}
+};
 
 exports.updateStudent = (req, res) => {
-
-  const {firstName, middleName, lastName, gender } = req.body;
-  const {id} = req.params;
+  const { firstName, middleName, lastName, gender } = req.body;
+  const { id } = req.params;
 
   try {
-    dbConnection.query("UPDATE student SET firstName=?, middleName=?, lastName=?, gender=?, class=? WHERE id=?",
-
-    [firstName, middleName, lastName, gender, req.body.class, id],
-        (err, results, fields) => {
+    dbConnection.query(
+      "UPDATE student SET firstName=?, middleName=?, lastName=?, gender=?, class=? WHERE id=?",
+      [firstName, middleName, lastName, gender, req.body.class, id],
+      (err, results, fields) => {
         if (err) res.status(417).send({ message: "Failed to edit Student" });
         if (results)
           res.status(200).send({ message: "Seccessfully edited a Student" });
@@ -82,4 +87,4 @@ exports.updateStudent = (req, res) => {
   } catch (err) {
     console.log(err);
   }
-}
+};
