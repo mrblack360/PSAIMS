d8aefba094b447d41ee4255807f9fb0929d8dca9
Maswi, MR <rmaswi360@outlook.com>
2021-08-21 21:53:14 +0300

    Change database configurations

--- Psaims/config/mysql.js
+++ Psaims/config/mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 var dbConnection = mysql.createConnection({
-  host: "sql5.freemysqlhosting.net",
-  user: "sql5419698",
-  password: "McdByWYmnW",
-  database: "sql5419698",
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b84e11d16af9f5",
+  password: "fe761177",
+  database: "heroku_39e7fcacf83506d",
 });
 
 module.exports =  dbConnection;
